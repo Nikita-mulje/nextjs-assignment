33a104546410d4e87f20046a4ab451f3be3d3596
Nikita <nikita.mulje@nonstopio.com>
2025-06-03 15:21:17 +0530

    refactor layout structure by integrating DashboardLayout into RootLayout; remove redundant layout files from Employees, Dashboard, and Settings sections

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+import DashboardLayout from "@/components/ui/layout/dashboard-layout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,8 +31,7 @@ export default function RootLayout({
       >
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
 
-          {children}
-        </ThemeProvider>
+        <DashboardLayout>{children}</DashboardLayout>        </ThemeProvider>
 
       </body>
 
